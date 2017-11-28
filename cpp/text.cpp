3fd4026d0363ad6315a2c096d26c9847e4fa7f41
HBat11233 <33086330+HBat11233@users.noreply.github.com>
2017-11-28 15:08:23 +0800

    1

---
+++ cpp/text.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    for(int i=0;i<16;++i)
+    {
+        for(int j=0;j<16;++j)
+        {
+            if(i==0||i==15||j==0||j==15)continue;
+            printf("(%2d,%2d) ",i,j);
+        }
+        printf("\n");
+    }
+    system("pause");    
+    return 0;
+}
