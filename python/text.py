cddbfce638fdcec7b07d46c75330eab317bc5994
HBat11233 <33086330+HBat11233@users.noreply.github.com>
2017-12-28 19:42:58 +0800

    阿斯顿

--- python/text.py
+++ python/text.py
@@ -3,8 +3,8 @@ import re
 with open('input.txt','r') as f:
 #with open('input.txt','w')
     all_num={}
-    for i in f.readlines():
-        listline=re.findall(r'[A-Za-z]+',i)
+    for i in f.readlines():#f.readlines()读入全部数据，返回列表，列表中的每一个元素是一行字符串   ；  ； ；  如果用f.readline()，是读入一行字符串，所以列表中的美一个元素是字母
+        listline=re.findall(r'[A-Za-z]+',i)#'[A-Za-z]'//匹配字母
         print(listline)
         for word in listline:
             word_count=len(re.findall(word,i))
