33e688d80ba17e0bb20b9e0ece1a0283807ee371
HBat11233 <33086330+HBat11233@users.noreply.github.com>
2017-12-28 19:40:51 +0800

    123

---
+++ python/lrean/2_3_4.py
@@ -0,0 +1,5 @@
+str1=" hjj jkk "
+print("-----"+str1+"-----")
+print("-----"+str1.rstrip()+"-----")#delete ' '  end
+print("-----"+str1.lstrip()+"-----")#delete ' '  begin
+print("-----"+str1.strip()+"-----")#delete ' '  all
---
+++ python/lrean/s2.py
@@ -0,0 +1,5 @@
+name="qwer asdfppASD"
+print(name.title())
+print(name.upper())
+print(name.lower())
+print(name)
