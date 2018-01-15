a20e3e161ac7d75ffe763189bbc1b89dfba41291
HBat11233 <33086330+HBat11233@users.noreply.github.com>
2018-01-15 17:54:33 +0800

    更新

--- cpp/Untitled-1.cpp
+++ cpp/Untitled-1.cpp
@@ -1,31 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define N 5
 
-using namespace std;
-
-typedef struct stu_type
-{ 
- char no[13];
- char name[20];
- int age;
-};
+typedef struct student
+	{
+		char no[13];
+		char name[20];
+		int age;
+	}STU;
 
 int main()
-{
-    stu_type stu[5];
-    for(int i=0;i<5;++i)
-    {
-        printf("student name:");
-        scanf("%s",stu[i].name);
-        printf("student age:");
-        scanf("%d",&stu[i].age);
-    }
-    int ans=0;
-    for(int i=0;i<5;++i)
-    {
-        if(stu[i].age>20)ans++;
-    }
-    printf("20old :%d",ans);
-    system("pause");
-    return 0;
-}
+	{
+		int inputin(struct student x[]);
+		int count(STU *p);
+		int people_num;
+		STU stu[N];
+		inputin(stu);
+		people_num=count(stu);
+		printf("输入的个人信息中，年龄在20岁以上的有%d人",people_num);
+		return 0; 
+	}
+	
+int count(STU *p)
+	{
+		int conter=0,i;
+		for(i=0;i<N;i++)
+			{
+				if(p[i].age>20)
+					{
+						conter++;
+					}
+			}
+		return conter;
+	}
+	
+int inputin(struct student x[])
+	{
+		int i,ages=0;
+		for(i=0;i<N;i++)
+			{
+				printf("请输入该学生的姓名:"); 
+				gets(x[i].name);
+				printf("请输入该学生的年龄:");
+				scanf("%d",&ages);
+				x[i].age=ages;
+			}
+            return 0;
+	}
+	
