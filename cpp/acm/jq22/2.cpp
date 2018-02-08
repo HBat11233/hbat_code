fba60e85e2befe85f59ab69f3271418d70d99938
HBat11233 <33086330+HBat11233@users.noreply.github.com>
2018-02-08 14:27:48 +0800

    dfs

---
+++ cpp/acm/jq22/2.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cstdlib>
+#include <cstring>
+
+#define xc tx+dx[i]
+#define yc ty+dy[i]
+
+using namespace std;
+
+int w,h;
+short maps[21][21];
+int bx,by,ex,ey;
+int minx=0x7fffffff;
+int ans=1;
+
+int dx[4]={0,0,1,-1};
+int dy[4]={1,-1,0,0};
+
+void dfs(int x,int y)
+{
+    if(ans>10)return;
+    for(int i=0;i<4;++i)
+    {
+        int tx=x+dx[i],ty=y+dy[i];
+        if(tx>=0&&tx<w&&ty>=0&&ty<h&&maps[ty][tx]==1)continue;
+        int k=0;
+        do
+        {
+            k++;
+            tx=x+dx[i]*k;
+            ty=y+dy[i]*k;
+            if(maps[ty][tx]==3)
+            {
+                minx=min(minx,ans);
+                return;
+            }
+        }while(xc>=0&&xc<w&&yc>=0&&yc<h&&maps[yc][xc]!=1);
+        if(xc>=0&&xc<w&&yc>=0&&yc<h&&maps[yc][xc]==1)
+        {
+            ans++;
+            maps[yc][xc]=0;
+            dfs(tx,ty);
+            maps[yc][xc]=1;
+            ans--;
+        }
+    }
+    return;
+}
+
+int main()
+{
+    while(scanf("%d%d",&w,&h),w+h)
+    {
+        memset(maps,0,sizeof(maps));
+        for(int i=0;i<h;++i)
+        {
+            for(int j=0;j<w;++j)
+            {
+                scanf("%d",*(maps+i)+j);
+                if(maps[i][j]==2)bx=j,by=i;
+                else if(maps[i][j]==3)ex=j,ey=j;
+            }
+        }
+        minx=0x7fffffff;
+        ans=1;
+        dfs(bx,by);
+        minx=minx>10? -1:minx;
+        printf("%d\n",minx);
+    }
+    return 0;
+}
---
+++ cpp/acm/jq22/3.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+int w,h;
+char maps[101][101];
+bool pmap[101][101];
+int dx[4]={0,0,1,-1};
+int dy[4]={1,-1,0,0};
+int ans=0;
+
+void dfs(int x,int y)
+{
+    for(int i=0;i<4;++i)
+    {
+        int tx=x+dx[i];
+        int ty=y+dy[i];
+        if(tx>=0&&tx<h&&tx>=0&&ty<h&&maps[tx][ty]==maps[x][y]&&!pmap[tx][ty])
+        {
+            pmap[tx][ty]=true;
+            dfs(tx,ty);
+        }
+    }
+}
+
+int main()
+{
+    while(scanf("%d%d",&h,&w),w+h)
+    {
+        memset(maps,0,sizeof(maps));
+        memset(pmap,0,sizeof(pmap));
+        for(int i=0;i<h;++i)
+        {
+            getchar();
+            for(int j=0;j<w;++j)
+            {
+                scanf("%c",*(maps+i)+j);
+            }
+        }
+        for(int i=0;i<h;++i)
+        {
+            for(int j=0;j<w;++j)
+            {
+                if(!pmap[i][j])
+                {
+                    pmap[i][j]=true;
+                    ans++;
+                    dfs(i,j);
+                }
+            }
+        }
+        printf("%d\n",ans);
+        ans=0;
+    }
+    return 0;
+}
